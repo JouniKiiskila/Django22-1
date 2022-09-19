80385c0d292da01df28f7c1f17202e30d1ffa9f9
JouniKiiskila <77729162+JouniKiiskila@users.noreply.github.com>
2022-09-19 15:20:58 +0300

    3

--- varauskalenteri/models.py
+++ varauskalenteri/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 class Tapahtuma(models.Model):
     otsikko = models.CharField(max_length=200)
@@ -10,5 +10,14 @@ class Tapahtuma(models.Model):
     def __str__(self):
         return f"{self.otsikko} ({self.alku}--{self.loppu})"
 
-    def kesto(self):
+    
+            
+
+    def kesto(self) -> datetime.timedelta:        
         return self.loppu - self.alku
+
+
+    def kesto_tuntia(self):
+        kesto = self.kesto()
+        kesto.days
+        return kesto.loppu - self.alku
